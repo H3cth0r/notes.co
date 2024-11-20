c7234a39ea0280371a3a80a4c4ed5f599fd5158d
h3cth0r <hectorsucre13@gmail.com>
2024-11-19 23:36:41 -0600

    added first code test in C to test C code

---
+++ tests/tests_c/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../notesco/c_src/markdown_to_html.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <input_file.md> <output_file.html>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* input = fopen(argv[1], "r");
+    if (!input) {
+        perror("Error opening input file");
+        return 1;
+    }
+
+    FILE* output = fopen(argv[2], "w");
+    if (!output) {
+        perror("Error opening output file");
+        fclose(input);
+        return 1;
+    }
+
+    fprintf(output, "<!DOCTYPE html>\n<html>\n<head>\n<title>Markdown to HTML</title>\n</head>\n<body>\n");
+
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), input)) {
+        parse_markdown_line(line, output);
+    }
+
+    fprintf(output, "</body>\n</html>\n");
+
+    fclose(input);
+    fclose(output);
+
+    printf("Markdown converted to HTML successfully.\n");
+    return 0;
+}
---
+++ tests/tests_c/note_test.md
@@ -0,0 +1,21 @@
+# Hello
+## This is the output
+
+Hi my friends
+
+I'm testing this
+
+**test_bold**: I'm *testing* this
+*two*: Now **testing** this
+
+- one
+    - **three**: result
+    - *three*: resulting
+- *four*: not getting listed
+    - *three*: resulting
+- Not getting listed
+
+1. another
+    1. haha
+    2. hoho
+2. Also Not getting listed
